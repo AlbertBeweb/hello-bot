5048cd3d9fb13b1963a3f2ea83878daf0666b8e7
Clement Denis <le.mikmac@gmail.com>
2017-04-15 02:33:29 +0200

    do the things

---
+++ bot.js
@@ -0,0 +1,22 @@
+const config = require('./config')
+const fs = require('fs')
+
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const EMMO = '262698796616646657'
+
+let rules = fs.readFileSync('./rules.md', 'utf8')
+
+bot.on('message', ({ author, guild, content }) => {
+  if (guild || author.id !== EMMO) return
+  fs.writeFile('./rules.md', rules = content, err => author.send(err
+    ? `Erreur en changeant les regles: \`${err.message}\``
+    : `regles mises a jour !`))
+})
+
+bot.on('guildMemberAdd', ({ user, guild }) => {
+  guild.defaultChannel.sendMessage(`:sparkles: Bienvenue sur le discord L'Ordre des Schlags <@${user.id}>! Je t'invite à lire les règles ainsi que le mode d'emploi de Discord dans le chan Infos,  à te présenter dans le salon General et faire connaissance avec tout le monde ! :sparkles:`)
+  user.send(rules).catch(console.error)
+})
+
+bot.login(config.botToken)
